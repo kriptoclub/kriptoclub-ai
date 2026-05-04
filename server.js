5e81eedc4634744539d7368fe3c66651d3995fae
kriptoclub <edvard.kurnik@gmail.com>
2026-05-04 16:57:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -209,7 +209,17 @@ Kot vedno pa moramo upoštevati tudi alternativni scenarij. V primeru padca pod 
  } else if (currentPrice < waveStart) {
 
     analysis = `
-    cena je tik pod prejšnjim vrhom
+    Zgodil se je pomemben premik in cena se trenutno nahaja v neposredni bližini ${Math.round(waveStart)} USD. Na tem območju je potrebna povečana previdnost, saj obstajata dva možna scenarija.
+
+1. Scenarij – dvojni vrh
+Obstaja možnost oblikovanja dvojnega vrha. Tudi če pride do rahlo višjega ali nižjega vrha in nato zavrnitve, to še vedno ohranja veljavnost tega vzorca.
+Vzorec je potrjen šele s padcem pod ${Math.round(waveEnd)} USD.
+
+2. Scenarij – preboj navzgor
+Če bo nivo ${Math.round(waveStart)} USD odločno presežen, se lahko začne razvijati nov impulzivni val. Prva pomembna tarča se nahaja pri ${Math.round(target1)} USD.
+Pred tem pa je potrebno spremljati še nivo ${Math.round(fib1382)} USD, ki predstavlja zadnjo pomembno oviro pred nadaljevanjem rasti.
+
+Kot vedno pa moramo upoštevati tudi alternativni scenarij. V primeru padca pod ${Math.round(invalidationLevel)} USD se scenarij rasti razveljavi.
 `;
  } else if (currentPrice < fib1382) {
 
